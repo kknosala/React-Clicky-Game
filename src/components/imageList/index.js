49fa38cfa3e4375695deec4ceb50f56dcc19456f
kknosala <51036712+kknosala@users.noreply.github.com>
2019-11-03 12:30:33 -0700

    images displaying, onClick working

--- src/components/imageList/index.js
+++ src/components/imageList/index.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import "./index.css";
+import Image from "../image";
+import images from "../../images.json";
 
 class ImageList extends Component {
+  state = {
+    images
+  };
+
+  handleClick = id => console.log(`Id is ${id}`);
+
   render() {
     return (
       <div className="imageList">
-        <p>Images Go here</p>
+        {this.state.images.map(images => (
+          <Image
+            handleClick={this.handleClick}
+            id={images.id}
+            key={images.id}
+            image={images.image}
+          />
+        ))}
       </div>
     );
   }
